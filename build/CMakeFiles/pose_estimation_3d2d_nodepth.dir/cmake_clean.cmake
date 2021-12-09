6de4ba72972420247cb2b7544aaa7240970ec986
JingJie_Huang <jack810435ge@gmail.com>
2021-12-09 15:42:54 +0800

    modified pose_estimation_3d2d_nodepth.cpp, fix adjacent image 3d pointcloud bug

---
+++ build/CMakeFiles/pose_estimation_3d2d_nodepth.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_estimation_3d2d_nodepth.dir/pose_estimation_3d2d_nodepth.cpp.o"
+  "CMakeFiles/pose_estimation_3d2d_nodepth.dir/pose_estimation_3d2d_nodepth.cpp.o.d"
+  "pose_estimation_3d2d_nodepth"
+  "pose_estimation_3d2d_nodepth.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_estimation_3d2d_nodepth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/pose_estimation_3d2d_nodepth.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pose_estimation_3d2d_nodepth.
+# This may be replaced when dependencies are built.
