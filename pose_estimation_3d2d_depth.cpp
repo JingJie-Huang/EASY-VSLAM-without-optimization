59d474d13866f3ff25d1d5588265defd35fa6c2e
JingJie_Huang <jack810435ge@gmail.com>
2021-12-16 18:02:23 +0800

    modified 3d2d.cpp file

--- pose_estimation_3d2d_depth.cpp
+++ pose_estimation_3d2d_depth.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
@@ -17,6 +18,9 @@
 
 using namespace std;
 using namespace cv;
+
+
+bool readYaml( Mat& cameraMatrix, Mat& distCoeffs );
 
 void find_feature_matches (
     const Mat& img_1, const Mat& img_2,
@@ -36,83 +40,114 @@ void bundleAdjustment (
 
 int main ( int argc, char** argv )
 {
+    Mat cameraMatrix, distCoeffs;
+    bool state;
+     
     if ( argc != 2 )
     {
         cout<<"usage: path_to_dataset"<<endl;
         return 1;
     }
+
+    state = readYaml(cameraMatrix, distCoeffs);
+    if ( !state ){
+        cout << "Error reading camera .yaml file" << endl;
+        return 1;
+    }
+
     string path_to_dataset = argv[1];
-    string associate_file = path_to_dataset + "/associate.txt";
+    string associate_file = path_to_dataset + "/associate_rgb_depth.txt";
 
     ifstream fin ( associate_file );
 
     string rgb_file, depth_file, time_rgb, time_depth;
-    cv::Mat color_pre, color_cur, depth_pre, depth_cur;
-    // 相机内参
+    cv::Mat color_prev, color_curr, color_undist, depth_prev, depth_curr;
+    /* Camera Intrinsic 
     float cx = 325.5;
     float cy = 253.5;
     float fx = 518.0;
     float fy = 519.0;
-    float depth_scale = 1000.0;
-    Eigen::Matrix3f K;
-    K << fx, 0.f, cx, 0.f, fy, cy, 0.f, 0.f, 1.0f;
-    
-    for(k=0; k<10; k++){
-        cout<<"*********** loop "<<index<<" ************"<<endl;
-        fin >> time_rgb >> rgb_file >> time_depth >> depth_file;
-        if(k == 0){
-            color_pre = cv::imread ( path_to_dataset+"/"+rgb_file, CV_LOAD_IMAGE_COLOR);
-            depth_pre = cv::imread ( path_to_dataset+"/"+depth_file, -1 );
-            continue;
-        }
-        if ( color.data==nullptr || depth.data==nullptr ){
-            continue;
-        }
-        color_cur = cv::imread ( path_to_dataset+"/"+rgb_file, CV_LOAD_IMAGE_COLOR);
-        depth_cur = cv::imread ( path_to_dataset+"/"+depth_file, -1 );
-
-            
-
-         
-    }
-    //-- 读取图像
-    Mat img_1 = imread ( argv[1], CV_LOAD_IMAGE_COLOR );
-    Mat img_2 = imread ( argv[2], CV_LOAD_IMAGE_COLOR );
-
+    float depth_scale = 5000.0;
+    */
     vector<KeyPoint> keypoints_1, keypoints_2;
     vector<DMatch> matches;
-    find_feature_matches ( img_1, img_2, keypoints_1, keypoints_2, matches );
-    cout<<"一共找到了"<<matches.size() <<"组匹配点"<<endl;
-	cout << keypoints_1.size()  << endl;
-    // 建立3D点
-    Mat d1 = imread ( argv[3], CV_LOAD_IMAGE_UNCHANGED );       // 深度图为16位无符号数，单通道图像
-    Mat K = ( Mat_<double> ( 3,3 ) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1 );
-    vector<Point3f> pts_3d;
-    vector<Point2f> pts_2d;
-    for ( DMatch m:matches )
-    {
-        ushort d = d1.ptr<unsigned short> (int ( keypoints_1[m.queryIdx].pt.y )) [ int ( keypoints_1[m.queryIdx].pt.x ) ];	
-		if ( d == 0 )   // bad depth
+    Mat r;
+    Mat t = ( Mat_<double> ( 3,1 ) << 0, 0, 0 );
+    Mat R = ( Mat_<double> ( 3,3 ) << 1.0, 0, 0, 0, 1.0, 0, 0, 0, 1.0 );
+
+    // initialization of translation and rotation
+
+
+
+    for(int k=0; k<10; k++){
+        cout << "*********** loop " << k << " ************" << endl;
+        fin >> time_rgb >> rgb_file >> time_depth >> depth_file;
+        if(k == 0){
+            color_prev = cv::imread ( path_to_dataset+"/"+rgb_file, CV_LOAD_IMAGE_COLOR);
+            undistort( color_prev, color_undist, cameraMatrix, distCoeffs, getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, color_prev.size(), 0, color_prev.size() ) );
+            color_prev = color_undist.clone();
+            depth_prev = cv::imread ( path_to_dataset+"/"+depth_file, -1 );
             continue;
-        float dd = d/5000.0;
-        Point2d p1 = pixel2cam ( keypoints_1[m.queryIdx].pt, K );
-        pts_3d.push_back ( Point3f ( p1.x*dd, p1.y*dd, dd ) );
-        pts_2d.push_back ( keypoints_2[m.trainIdx].pt );
+        }    
+        color_curr = cv::imread ( path_to_dataset+"/"+rgb_file, CV_LOAD_IMAGE_COLOR);
+        undistort( color_curr, color_undist, cameraMatrix, distCoeffs, getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, color_curr.size(), 0, color_curr.size() ) );
+        color_curr = color_undist.clone();
+        depth_curr = cv::imread ( path_to_dataset+"/"+depth_file, -1 );
+
+        find_feature_matches ( color_prev, color_curr, keypoints_1, keypoints_2, matches );
+        cout << "There are total: " << matches.size() << "sets of pairs" << endl;
+        cout << keypoints_1.size()  << endl;
+        // 建立3D点
+        //Mat d1 = imread ( argv[3], CV_LOAD_IMAGE_UNCHANGED );       // 深度图为16位无符号数，单通道图像
+        //Mat K = ( Mat_<double> ( 3,3 ) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1 );
+        vector<Point3f> pts_3d;
+        vector<Point2f> pts_2d;
+        
+
+        for ( DMatch m:matches )
+        {
+            ushort d = depth_prev.ptr<unsigned short> (int ( keypoints_1[m.queryIdx].pt.y )) [ int ( keypoints_1[m.queryIdx].pt.x ) ];	
+            if ( d == 0 )   // bad depth
+                continue;
+            float dd = d/5000.0;
+            Point2d p1 = pixel2cam ( keypoints_1[m.queryIdx].pt, cameraMatrix );
+            // transform point cloud from current frame to global frame
+            Mat pts_3d_tran = ( Mat_<double>(3,1) << p1.x*dd-t.at<double>(0,0), p1.y*dd-t.at<double>(1,0), dd-t.at<double>(2,0) );
+            //cout << "pts_3d_tran = " << pts_3d_tran << endl;
+            pts_3d_tran = R.t()*pts_3d_tran;
+            //cout << "pts_3d_tran = " << pts_3d_tran << endl;
+            //pts_3d.push_back ( Point3f ( p1.x*dd, p1.y*dd, dd ) );
+            pts_3d.push_back ( Point3f ( pts_3d_tran.at<double>(0,0), pts_3d_tran.at<double>(1,0), pts_3d_tran.at<double>(2,0) ) );
+            pts_2d.push_back ( keypoints_2[m.trainIdx].pt );
+        }
+
+        cout<<"3d-2d pairs: "<<pts_3d.size() <<endl;
+
+        
+        solvePnPRansac( pts_3d, pts_2d, cameraMatrix, Mat(), r, t, false );
+        
+        cv::Rodrigues ( r, R ); // Rodrigues to rotation matrix
+
+        cout << "R = " << endl << R << endl;
+        cout << "R transpose = " << R.t() << endl;
+        cout << "t=" << endl << t << endl;
+
+        cout<<"calling bundle adjustment"<<endl;
+
+        bundleAdjustment ( pts_3d, pts_2d, cameraMatrix, R, t );            
+
+
+        // swap state
+        color_prev = color_curr.clone();
+        depth_prev = depth_curr.clone();
+        // reset
+        keypoints_1.clear(); 
+        keypoints_2.clear();
+        matches.clear(); 
     }
 
-    cout<<"3d-2d pairs: "<<pts_3d.size() <<endl;
 
-    Mat r, t;
-    solvePnP ( pts_3d, pts_2d, K, Mat(), r, t, false ); // 调用OpenCV 的 PnP 求解，可选择EPNP，DLS等方法
-    Mat R;
-    cv::Rodrigues ( r, R ); // r为旋转向量形式，用Rodrigues公式转换为矩阵
-
-    cout<<"R="<<endl<<R<<endl;
-    cout<<"t="<<endl<<t<<endl;
-
-    cout<<"calling bundle adjustment"<<endl;
-
-    bundleAdjustment ( pts_3d, pts_2d, K, R, t );
+    
 }
 
 void find_feature_matches ( const Mat& img_1, const Mat& img_2,
@@ -153,18 +188,44 @@ void find_feature_matches ( const Mat& img_1, const Mat& img_2,
         if ( dist > max_dist ) max_dist = dist;
     }
 
-    printf ( "-- Max dist : %f \n", max_dist );
-    printf ( "-- Min dist : %f \n", min_dist );
+    //printf ( "-- Max dist : %f \n", max_dist );
+    //printf ( "-- Min dist : %f \n", min_dist );
 
     //当描述子之间的距离大于两倍的最小距离时,即认为匹配有误.但有时候最小距离会非常小,设置一个经验值30作为下限.
     for ( int i = 0; i < descriptors_1.rows; i++ )
     {
-        if ( match[i].distance <= max ( 2*min_dist, 30.0 ) )
+        if ( match[i].distance <= max ( 2*min_dist, 25.0 ) )
         {
             matches.push_back ( match[i] );
         }
     }
 }
+
+
+bool readYaml( Mat& cameraMatrix, Mat& distCoeffs ) {
+    std::string filename = "../camera_calibration_parameters.yaml";
+    FileStorage fs(filename, FileStorage::READ);
+    if (!fs.isOpened()) {
+        cout << "failed to open file " << filename << endl;
+        return false;
+    }
+    
+    fs["cameraMatrix"] >> cameraMatrix;
+    fs["distCoeffs"] >> distCoeffs;
+    cout << "Reading .yaml file ..." << endl << endl;
+    cout << "cameraMatrix = \n" << cameraMatrix << endl;
+    cout << "distCoeffs = \n" << distCoeffs << endl;
+ 
+    // read string
+    string timeRead;
+    fs["calibrationDate"] >> timeRead;
+    cout << "calibrationDate = " << timeRead << endl;
+    cout << "Finish reading .yaml file" << endl;
+
+    fs.release();
+    return true;
+}
+
 
 Point2d pixel2cam ( const Point2d& p, const Mat& K )
 {
